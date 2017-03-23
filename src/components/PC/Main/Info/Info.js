aa69f966e4ee4d1f855da5b65032de5f76ee75b9
kusob <boseokjung@gmail.com>
2017-03-23 13:58:29 +0900

    6 PC Main

---
+++ src/components/PC/Main/Info/Info.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styles from './Info.css';
+
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url:'https://openlibrary.org/recentchanges.json?limit=5',
+      context:this,
+      dataType:'json',
+      type:'GET',
+      success: function(result){
+        console.log(result);
+        this.setState({
+          comment1:result[0].comment,
+          comment2:result[1].comment,
+          comment3:result[2].comment,
+          comment4:result[3].comment,
+          comment5:result[4].comment,
+          kind:result[0].kind,
+          time:result[0].timestamp
+        });
+      }
+    });
+  }
+
+  render() {
+    var comment1 = this.state.comment1;
+    var comment2 = this.state.comment2;
+    var comment3 = this.state.comment3;
+    var comment4 = this.state.comment4;
+    var comment5 = this.state.comment5;
+    var kind = this.state.kind;
+    var time = this.state.time;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.title}>
+          대회정보<br/>
+          <span className={styles.date}>{time}</span>
+        </div>
+        <img
+          onClick={this.search}
+          src={require('./Img/arrow.png')}
+          className={styles.arrowLeft}/>
+        <div className={styles.box}>
+          <div className={styles.team}>
+            <div className={styles.teamName}>{kind}</div>
+            <span className={styles.info}>
+              <div>{comment1}</div>
+              <div>{comment2}</div>
+              <div>{comment3}</div>
+              <div>{comment4}</div>
+              <div>{comment5}</div>
+            </span>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.team}>
+            <div className={styles.teamName}>{kind}</div>
+            <span className={styles.info}>
+              <div>{comment1}</div>
+              <div>{comment2}</div>
+              <div>{comment3}</div>
+              <div>{comment4}</div>
+              <div>{comment5}</div>
+            </span>
+          </div>
+        </div>
+        <img
+          onClick={this.search}
+          src={require('./Img/arrow.png')}
+          className={styles.arrowRight}/>
+      </div>
+    );
+  }
+}
+
+export default Info;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Top from './components/Top/Top';
-import App from './components/App/App';
-import Info from './components/Info/Info';
-import Footer from './components/Footer/Footer';
-
-var data = [{ "when": "2 minutes ago",
-              "who": "Jilaal Dupre",
-              "description": "Created new account"
-            },
-            {
-              "when": "1 hour ago",
-              "who": "Losaae White",
-              "description": "Added fist chapter"
-            }];
-var headings = ['When', 'Who', 'Description'];
-var props = { headings:headings, changeSets:data }
+import Top from './components/PC/Main/Top/Top';
+import Title from './components/PC/Main/Title/Title';
+import Info from './components/PC/Main/Info/Info';
+import Footer from './components/PC/Main/Footer/Footer';
 
 ReactDOM.render(<Top/>, document.getElementById('top'));
-ReactDOM.render(<App {...props} />, document.getElementById('app'));
+ReactDOM.render(<Title/>, document.getElementById('title'));
 ReactDOM.render(<Info/>, document.getElementById('info'));
 ReactDOM.render(<Footer/>, document.getElementById('footer'));
