b6d739d14826ceaef1b16b98160e35f277413946
jungboseok <boseokjung@gmail.com>
2017-05-22 17:24:45 +0900

    커뮤니티 글쓰기v1

--- src/components/Community/CommunityWrite.js
+++ src/components/Community/CommunityWrite.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Common from '../Common/js/Common.js';
 import styles from './CommunityWrite.css';
-
+import FileInput from 'react-file-input';
 class CommunityWrite extends Component {
   constructor(props) {
       super(props);
@@ -15,9 +15,40 @@ class CommunityWrite extends Component {
     return(
       <div>
         <div className={styles.container}>
-          <input type="text" placeholder="제목" className={styles.anonymForm}/>
-          <textarea placeholder="내용을 입력하세요" className={styles.anonymTextarea}/>
-          <img src={require('../Common/Img/write.png')} className={styles.submitBtn} onClick={this.write}/>
+          <form encType="multipart/form-data">
+          <table className={styles.comw_tab}>
+            <thead>
+              <tr>
+                <td className={styles.comw_td1}>
+                  <img src={require('../Common/img/back.png')} className={styles.comw_backBtn} onClick={Common.back}/>
+                  <img src={require('../Common/img/write.png')} className={styles.comw_writeBtn} onClick={Common.back}/>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                    <input type="text" placeholder="제목" className={styles.comw_titleForm}/>
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                    <textarea placeholder="내용을 입력하세요" className={styles.comw_textarea}/>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FileInput name="files"
+                    accept=".png,.gif,.pptx,.js"
+                    placeholder="클릭해서 파일을 첨부하세요"
+                    className={styles.comw_inputFile}
+                    onChange={this.handleChange}
+                    multiple />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          </form>
         </div>
       </div>
     );
