926fd9b2ba797a41221842d984caa7d5245626e8
jungboseok <boseokjung@gmail.com>
2017-06-02 14:55:19 +0900

    웹스톰으로 갈아탔다! 그리고 메인화면 게임정보 표시 +오늘 진행하는 경기가 없으면 대회가 없다고 표시

--- src/components/Main/Info/Info.js
+++ src/components/Main/Info/Info.js
@@ -1,90 +1,100 @@
 import React, {Component} from 'react';
 import Common from '../../Common/js/Common.js';
 import styles from './Info.css';
-import Champions from "../../search/Champion/Champions";
-
 
 class Info extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      gamelist:[]
-    };
-  }
+        this.state = {
+            gamelist: [],
+            now: ""
+        };
+    }
 
-  componentDidMount() {
-    this.getGameInfo();
-  }
+    componentDidMount() {
+        this.getGameInfo();
+    }
 
-  getGameInfo(){
-      var addr = Common.getApi();
-      return $.getJSON(addr+'api/Home/gameInfo')
-          .then((data) => {
-              this.setState({
-                  gamelist : data
-              });
-          });
-  }
+    getGameInfo() {
+        var addr = Common.getApi();
+        return $.getJSON(addr + 'api/Home/gameInfo')
+            .then((data) => {
 
-  leftArrow(){
-      alert('left');
-  }
-
-  rightArrow(){
-      alert('right');
-  }
-
-  render() {
-    var leftTeam = this.state.leftTeam;
-    var rightTeam = this.state.rightTeam;
-    var status = this.state.status;
-    var time = this.state.gameTime;
-
-    return (
-      <div className={styles.container}>
-        <div className={styles.title}>
-          대회정보<br/>
-          <img
-            onClick={this.leftArrow}
-            src={require('./Img/arrow.png')}
-            className={styles.arrowLeft}/>
-          <span className={styles.date}>{time}</span>
-            <img
-              onClick={this.rightArrow}
-              src={require('./Img/arrow.png')}
-              className={styles.arrowRight}/>
-        </div>
-          {this.state.gamelist.map((list, i) => {
-              return(
-                  <ChampionshipInfo key={i}
-                                    left={list.leftTeam}
-                                    right={list.rightTeam}
-                                    status={list.status}
-                  />
-              );
-            })
-          }
+                this.setState({
+                    gamelist: data,
+                    now: ""
+                });
 
 
-      </div>
-    );
-  }
+            });
+    }
+
+    leftArrow() {
+        alert('left');
+    }
+
+    rightArrow() {
+        alert('right');
+    }
+
+    render() {
+        var message = "";
+        if(this.state.gamelist.length==0){
+            message+="오늘 경기는 없습니다";
+        }
+        return (
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    대회정보<br/>
+                    <img
+                        onClick={this.leftArrow}
+                        src={require('./Img/arrow.png')}
+                        className={styles.arrowLeft}/>
+                    <span className={styles.date}>{this.state.now}</span>
+                    <img
+                        onClick={this.rightArrow}
+                        src={require('./Img/arrow.png')}
+                        className={styles.arrowRight}/>
+                </div>
+                <span className={styles.nomsg}>
+                    {message}
+                </span>
+                {
+                    this.state.gamelist.map((list, i) => {
+                        return (
+                            <ChampionshipInfo key={i}
+                                              left={list.leftTeam}
+                                              right={list.rightTeam}
+                                              status={list.status}
+                                              gameTime={list.gameTime}
+                            />
+                        );
+                    })
+                }
+
+
+            </div>
+        );
+    }
 }
 
 class ChampionshipInfo extends Component {
-  render(){
-    return(
-        <div className={styles.box}>
-          <div className={styles.team}>
-            <div className={styles.teamName}>{this.props.left} vs {this.props.right}</div>
-            <span className={styles.info}>
-              <div>{this.props.status}</div>
-            </span>
-          </div>
-        </div>
-    );
-  }
+    render() {
+
+        return (
+            <div className={styles.box}>
+                <div className={styles.team}>
+                    <div className={styles.teamName}>
+                        {this.props.left} vs {this.props.right}
+                    </div>
+                    <span className={styles.info}>
+                        <div>{this.props.status}</div>
+                    </span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Info;
