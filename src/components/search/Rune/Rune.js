988165d25830e3b43b6a756b0913207b9fe4d281
jungboseok <boseokjung@gmail.com>
2017-06-11 22:12:34 +0900

    api만 받아서 데이터바인딩만하면 rune page는 이제 끝!

--- src/components/search/Rune/Rune.js
+++ src/components/search/Rune/Rune.js
@@ -7,11 +7,21 @@ class Rune extends Component {
 
         this.state = {
             runeList:[
-                { runeName : "리신"},
-                { runeName : "엘리스"}
+                {
+                    runeName : "리신",
+                    runeDetail:"공격력 +8.5  방어력 +9  마법저항력 +13  공격 속도 % +13.5%",
+                },
+                {
+                    runeName : "엘리스",
+                    runeDetail:"마법관통력 +6.8  방어력 +9  재사용 대기시간 % -7.5%  공격 속도 % +13.5%",
+                }
             ],
             currentRune:1
         };
+    }
+
+    runeClick(i){
+        this.setState({currentRune:i});
     }
 
     render(){
@@ -24,7 +34,8 @@ class Rune extends Component {
                                 {this.state.runeList.map((list, i) => {
                                     if(this.state.currentRune==i+1){
                                         return (
-                                            <td key={i} className={styles.rune_currentRune}>
+                                            <td
+                                                key={i} className={styles.rune_currentRune}>
                                                 <RuneList
                                                     runeName={list.runeName}
                                                     n={i+1}
@@ -32,7 +43,8 @@ class Rune extends Component {
                                             </td>);
                                     } else {
                                         return (
-                                            <td key={i} className={styles.rune_runeLists}>
+                                            <td onClick={()=>this.runeClick(i+1)}
+                                                key={i} className={styles.rune_runeLists}>
                                                 <RuneList
                                                     runeName={list.runeName}
                                                     n={i+1}
@@ -49,15 +61,19 @@ class Rune extends Component {
                         <tr>
                             <td className={styles.rune_content}>
                                 <div>
-                                    <span className={styles.rune_cont_runeName}>AD</span>
-                                    <span className={styles.rune_cont_runeDetail}>공격력 +8.5  방어력 +9  재사용 대기시간 % -7.5%  공격 속도 % +13.5%</span>
+                                    <span className={styles.rune_cont_runeName}>
+                                        {this.state.runeList[this.state.currentRune-1].runeName}
+                                    </span>
+                                    <span className={styles.rune_cont_runeDetail}>
+                                        {this.state.runeList[this.state.currentRune-1].runeDetail}
+                                    </span>
                                 </div>
                                 <div className={styles.rune_cont_hr}>
                                     <br/>
                                     <hr/>
                                     <div className={styles.rune_png}>
-                                        asdfasdf
-                                        <img src={require('./rune.png')}/>
+                                        asdfasdfdmdkdk으아아아아아아아아아 여기에 왜 이미지가 안들ㅇ어갈까아아아아아아아아아아아아아아아아아아아아
+                                        <img src={require('./rune.png')} />
                                     </div>
                                 </div>
                             </td>
