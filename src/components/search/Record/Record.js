a224b3cfce29d4741da1c9fc494e797eb561e9e0
jungboseok <boseokjung@gmail.com>
2017-06-12 22:36:29 +0900

    최근경기기록 페이지 ++

--- src/components/search/Record/Record.js
+++ src/components/search/Record/Record.js
@@ -9,7 +9,9 @@ class Record extends Component {
             recordList:[
                 {
                     id:1,
-                    champName:"럼블",
+                    champName:"Rumble",
+                    spell1:"Teleport",
+                    spell2:"Flash",
                     k:11,
                     d:1,
                     a:9,
@@ -21,19 +23,19 @@ class Record extends Component {
                     killinv:66,
                     itemlist:[
                         {
-                            itemid:5,
+                            itemid:3001,
                             name:"닌자의 신발"
                         },
                         {
-                            itemid:6,
+                            itemid:3009,
                             name:"빙하의 장막"
                         },
                         {
-                            itemid:7,
+                            itemid:3047,
                             name:"BF 대검"
                         },
                         {
-                            itemid:8,
+                            itemid:3071,
                             name:"가면"
                         },
                     ],
@@ -41,60 +43,62 @@ class Record extends Component {
                         {
                             playerid:1,
                             name:"시기받기좋을시기",
-                            champ:"신지드"
+                            champ:"Singed"
                         },
                         {
                             itemid:2,
                             name:"군대가야되젠장",
-                            champ:"리신"
+                            champ:"LeeSin"
                         },
                         {
                             itemid:3,
                             name:"류종하",
-                            champ:"쓰레쉬"
+                            champ:"Ekko"
                         },
                         {
                             itemid:4,
                             name:"woncon",
-                            champ:"루시안"
+                            champ:"Lucian"
                         },
                         {
                             itemid:5,
                             name:"bvest",
-                            champ:"피즈"
+                            champ:"Nami"
                         }
                     ],
                     opplist:[
                         {
                             playerid:1,
                             name:"성철이",
-                            champ:"럼블"
+                            champ:"Rumble"
                         },
                         {
                             itemid:2,
                             name:"양민학살용",
-                            champ:"리신"
+                            champ:"LeeSin"
                         },
                         {
                             itemid:3,
                             name:"보석",
-                            champ:"벨코즈"
+                            champ:"Velkoz"
                         },
                         {
                             itemid:4,
                             name:"뚜우뿌뚜",
-                            champ:"베인"
+                            champ:"Graves"
                         },
                         {
                             itemid:5,
                             name:"maruYeah",
-                            champ:"그브"
+                            champ:"Nami"
                         }
                     ],
                 },
                 {
                     id:2,
-                    champName:"리신",
+                    champName:"LeeSin",
+                    spell1:"Smite",
+                    spell2:"Flash",
                     k:19,
                     d:3,
                     a:18,
@@ -106,15 +110,15 @@ class Record extends Component {
                     killinv:80,
                     itemlist:[
                         {
-                            itemid:1,
+                            itemid:1033,
                             name:"요우무"
                         },
                         {
-                            itemid:2,
+                            itemid:1036,
                             name:"칠흑의 양날도끼"
                         },
                         {
-                            itemid:3,
+                            itemid:1054,
                             name:"헤르메스의 발걸음"
                         }
                     ],
@@ -122,54 +126,54 @@ class Record extends Component {
                         {
                             playerid:1,
                             name:"시기받기좋을시기",
-                            champ:"신지드"
+                            champ:"Singed"
                         },
                         {
                             itemid:2,
                             name:"군대가야되젠장",
-                            champ:"리신"
+                            champ:"LeeSin"
                         },
                         {
                             itemid:3,
                             name:"류종하",
-                            champ:"쓰레쉬"
+                            champ:"Karma"
                         },
                         {
                             itemid:4,
                             name:"woncon",
-                            champ:"루시안"
+                            champ:"Twitch"
                         },
                         {
                             itemid:5,
                             name:"bvest",
-                            champ:"피즈"
+                            champ:"Velkoz"
                         }
                     ],
                     opplist:[
                         {
                             playerid:1,
                             name:"성철이",
-                            champ:"럼블"
+                            champ:"Graves"
                         },
                         {
                             itemid:2,
                             name:"양민학살용",
-                            champ:"리신"
+                            champ:"Lucian"
                         },
                         {
                             itemid:3,
                             name:"보석",
-                            champ:"벨코즈"
+                            champ:"Twitch"
                         },
                         {
                             itemid:4,
                             name:"뚜우뿌뚜",
-                            champ:"베인"
+                            champ:"Karma"
                         },
                         {
                             itemid:5,
                             name:"maruYeah",
-                            champ:"그브"
+                            champ:"Karma"
                         }
                     ],
                 },
@@ -185,6 +189,8 @@ class Record extends Component {
                         {this.state.recordList.map((list, i) => {
                             return (<RecordList num={list.id}
                                                champName={list.champName}
+                                               spell1={list.spell1}
+                                               spell2={list.spell2}
                                                kda={list.k+"/"+list.d+"/"+list.a}
                                                avg={(list.k+list.a)/list.d}
                                                oncekill={list.oncekill}
@@ -208,18 +214,44 @@ class Record extends Component {
 }
 
 class RecordList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
     render(){
+        const champUrl = "img/champ/";
+        const jpg = ".jpg";
+        const champName =  champUrl + this.props.champName+jpg;
+
+        const spellUrl = "img/spell/Summoner";
+        const spell1 =  spellUrl + this.props.spell1+jpg;
+        const spell2 =  spellUrl + this.props.spell2+jpg;
+
+
         if(this.props.itemlist.length!=6){
             for(var i=0; i<6; i++){
                 if(this.props.itemlist[i]==null){
-                    this.props.itemlist.push({name:"아이템없음"});
+                    this.props.itemlist.push({
+                        itemid:"non",
+                        name:"아이템없음"
+                    });
                 }
             }
         }
         return (
             <tr className={styles.record_listTr}>
                 <td>
-                    <span>{this.props.champName}</span>
+                    <span>
+                        <img src={require("../"+champName)}/>
+                    </span>
+                </td>
+                <td>
+                    <span><img src={require("../"+spell1)}/></span><br/>
+                    <span><img src={require("../"+spell2)}/></span>
                 </td>
                 <td>
                     <div>
@@ -236,19 +268,21 @@ class RecordList extends Component {
                 </td>
                 <td>
                     {this.props.itemlist.map((ilist,i) => {
+                        const itemUrl = "img/item/";
+                        const itemImg = itemUrl + ilist.itemid + ".jpg";
                         if(i==3){
                             return (
                                 <span key={i}>
                                     <br/>
                                     <span className={styles.record_itemlist}>
-                                        {ilist.name}
+                                        <img src={require("../"+itemImg)} className={styles.record_itemList_itemImg}/>&nbsp;
                                     </span>
                                 </span>
                             );
                         } else {
                             return (<span className={styles.record_itemlist}
                                           key={i}>
-                                        {ilist.name}
+                                        <img src={require("../"+itemImg)} className={styles.record_itemList_itemImg}/>&nbsp;
                                 </span>
                             );
                         }
@@ -256,24 +290,26 @@ class RecordList extends Component {
                     })
                     }
                 </td>
-                <td>
+                <td  className={styles.record_playerlist}>
                     <div>
                         {this.props.playerlist.map((plist,i) => {
-                                return (<div className={styles.record_itemlist}
+                            const playerChampUrl = "img/champ/"+plist.champ+".jpg";
+                                return (<div
                                               key={i}>
-                                        {plist.champ} | {plist.name}
+                                        <img src={require("../"+playerChampUrl)} className={styles.record_playerList_champImg}/>{plist.name}
                                     </div>
                                 );
                         })
                         }
                     </div>
                 </td>
-                <td>
+                <td className={styles.record_playerlist}>
                     <div>
                         {this.props.opplist.map((plist,i) => {
-                            return (<div className={styles.record_itemlist}
+                            const oppChampUrl = "img/champ/"+plist.champ+".jpg";
+                            return (<div
                                          key={i}>
-                                    {plist.champ} | {plist.name}
+                                    <img src={require("../"+oppChampUrl)} className={styles.record_playerList_champImg}/>{plist.name}
                                 </div>
                             );
                         })
