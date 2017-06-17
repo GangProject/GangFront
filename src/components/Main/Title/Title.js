96f842bb89c78564573b5b8e6f52911173a263c1
jungboseok <boseokjung@gmail.com>
2017-06-17 16:40:05 +0900

    main페이지 텍스트박스 자동커서와 엔터키로 검색가능

--- src/components/Main/Title/Title.js
+++ src/components/Main/Title/Title.js
@@ -9,10 +9,16 @@ class Title extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             header: "GA.NG",
             data:"",
         };
+
+    }
+
+    componentDidMount() {
+        $('#userName').focus();
     }
 
     search(){
@@ -24,13 +30,24 @@ class Title extends Component {
         }
     }
 
+    pressEnter(event){
+        if(event.key == 'Enter') {
+            if($('#userName').val()==null||$('#userName').val()==""){
+                alert('소환사 이름을 입력하세요');
+
+            } else {
+                location.href="search?userName="+$('#userName').val();
+            }
+        }
+    }
+
     render() {
         return (
             <div>
                 <div className={styles.container}>
                   <div className={styles.header}>{this.state.header}</div>
-                  <input type="text" placeholder="소환사 이름" id="userName"  className={styles.searchForm}/>
-                      <img src={require('./Img/search.png')} onClick={this.search} className={styles.searchBtn}/>
+                  <input type="text" placeholder="소환사 이름" onKeyPress={this.pressEnter} id="userName" className={styles.searchForm} autofocus/>
+                  <img src={require('./Img/search.png')} onClick={this.search} className={styles.searchBtn}/>
                 </div>
             </div>
         );
